d63d82d9ebcd14215b225697f073f56ce1af255c
JadenSimon <sijaden@amazon.com>
2022-06-30 14:59:33 -0700

    Remove the majority of MDE references

--- scripts/build/generateServiceClient.ts
+++ scripts/build/generateServiceClient.ts
@@ -215,10 +215,6 @@ ${fileContents}
             serviceName: 'ClientTelemetry',
         },
         {
-            serviceJsonPath: 'types/mde-2020-04-05.normal.json',
-            serviceName: 'ClientMde',
-        },
-        {
             serviceJsonPath: 'src/codewhisperer/client/service-2.json',
             serviceName: 'CodeWhispererClient',
         },
