4aca7504609a7365464bab14b68b022af5368917
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2022-09-13 09:48:29 -0700

    feat(caws): use sono token on disk if available (#587)

--- src/caws/model.ts
+++ src/caws/model.ts
@@ -120,6 +120,8 @@ export async function autoConnect(authProvider: CawsAuthenticationProvider) {
             getLogger().debug(`quokka.codes: unable to auto-connect with user "${account.label}": %O`, err)
         }
     }
+
+    return authProvider.tryLoginFromDisk()
 }
 
 export function createClientFactory(authProvider: CawsAuthenticationProvider): () => Promise<CawsClient> {
